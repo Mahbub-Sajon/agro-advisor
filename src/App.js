f0b6229165ad9a37d33ebbb8f40f9902dc58bd5d
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-18 02:23:43 +0600

    all routes connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './component/About/About';
+import Blogs from './component/Blogs/Blogs';
+import CheckOut from './component/CheckOut/CheckOut';
+import Home from './component/Home/Home';
+import Login from './component/Login/Login';
 import Navigation from './component/Navigation/Navigation';
+import SignUp from './component/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
      <Navigation></Navigation>
+     <Routes>
+       <Route path="/" element={<Home></Home>}></Route>
+       <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+       <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
+       <Route path="/signup" element={<SignUp></SignUp>}></Route>
+       <Route path="/login" element={<Login></Login>}></Route>
+       <Route path="/about" element={<About></About>}></Route>
+     </Routes>
     </div>
   );
 }
--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -11,7 +11,8 @@ const Navigation = () => {
                { open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
             </div>
            <div className='md:flex justify-center'>
-            <img className=' rounded-md w-16 h-16' src={img} alt="" />
+           <img className=' rounded-md w-16 h-16' src={img} alt="" />
+            
            <nav className={`md:flex justify-center bg-lime-200 w-full duration-500 ease-in absolute md:static ${open ? 'top-6' : 'top-[-120px]'}`}>
                 <div className='mr-16'><CustomLink to="/">HOME</CustomLink></div>
                <div className='mr-16'> <CustomLink to="/blogs">BLOGS</CustomLink></div>
