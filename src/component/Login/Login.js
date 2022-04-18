896cf8be1f8cfe56faf845485b720500a5ce34d7
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-18 15:54:52 +0600

    login updated

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+   
+    
+    const [
+        signInWithEmailAndPassword,
+        user,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      const navigate = useNavigate();
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    if(user){
+        navigate('/');
+    }
+
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email,password);
+    }
+
+
     return (
         <div className='w-2/3  border-2 border-green-800 rounded-md mx-auto my-2'>
            <h1 className='text-3xl font-bold mb-4'>Login</h1>
 
-          <form>
+          <form onSubmit={handleUserSignIn}>
 
           <div className="input-group">
                <label className='block text-xl' htmlFor="email">Email</label>
-               <input className='w-3/4 h-12 border border-black mb-4 rounded-md' required type="email" name='email' />
+               <input onBlur={handleEmailBlur} className='w-3/4 h-12 border border-black mb-4 rounded-md' required type="email" name='email' />
                </div>
 
 
                <div className="input-group">
                <label className='block text-xl' htmlFor="password">Password</label>
-               <input className='w-3/4 h-12 border border-black mb-4 rounded-md' required type="password" name='password' />
+               <input onBlur={handlePasswordBlur} className='w-3/4 h-12 border border-black mb-4 rounded-md' required type="password" name='password' />
                </div>
+               <p className='text-red-800'>{error?.message}</p>
 
             <input className='w-32 h-12 bg-green-400 rounded-md mb-4 font-bold hover:bg-lime-500 cursor-pointer' type="submit" value="Login" />
 
